63929fe7ea235158b5c5d80cdc4066bc1a6c208a
Mpho Mamphoke <mphomamphoke@outlook.com>
2022-04-08 00:01:55 +0300

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	string myString =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char myString =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	fprintf(stderr, "%s\n", myString);
 	return (1);
 }
