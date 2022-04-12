d44c79a26e8a750183d39c0ef03e755ee66fab74
Mpho Mamphoke <mphomamphoke@outlook.com>
2022-04-12 19:14:21 +0300

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,12 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
+ * Description: prints _putchar
  * Return: On success 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-void main(void)
+int main(void)
 {
 	_putchar();
 
