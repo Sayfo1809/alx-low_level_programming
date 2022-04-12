d09656e22031a10ca9b286c375102e1fa584d73c
Mpho Mamphoke <mphomamphoke@outlook.com>
2022-04-12 21:58:49 +0300

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,11 @@
 
 int print_last_digit(int n)
 {
+  in lastDigit = n % 10;
 	if (n < 0)
 		n *= -1;
-
        	_putchar('0' + (n % 10));
+	_putchar('\n');
 
-	return (n % 10);
+	return lastDigit;
 }
